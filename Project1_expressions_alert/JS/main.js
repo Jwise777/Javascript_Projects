79d19f291a3badf26e42507662bbfed8966292ee
Jwise777 <109560319+Jwise777@users.noreply.github.com>
2022-08-18 22:15:25 -0400

    website

--- Project1_expressions_alert/JS/main.js
+++ Project1_expressions_alert/JS/main.js
@@ -1,7 +1,7 @@
   window.alert("Hello, World!"); //Adding window alert
     var A = "This is a string"; //Assigning a variable
     alert(A);
-    document.write("Lisa told Bart, \"Kncok it off, Bart! or I'll tell dad!\" //A string with quotations\"Eat my shorts!\" Bart responded. ");
+    document.write("Lisa told Bart, \"Kncok it off, Bart! or I'll tell dad!\"Eat my shorts!\" Bart responded. ");  //A string with quotations
     var Sent1 = "This is the beginning of the string"
     var Sent2 = "and this is the end of the string"
     document.write(Sent1
