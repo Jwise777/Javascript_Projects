a62a88d13d0328e26fdcae06c702ecf071498efe
Jwise777 <109560319+Jwise777@users.noreply.github.com>
2022-08-20 00:47:49 -0400

    comments add

--- Project5_comparisons_type_coercion/JS/main.js
+++ Project5_comparisons_type_coercion/JS/main.js
@@ -1,33 +1,33 @@
-document.write(typeof 7);
-document.write("40" + 10);
-function my_Function() {
+document.write(typeof 7); //displaying the type of variable 7 is
+document.write("40" + 10); //Showing ability to add 2 dffierent types of variables
+function my_Function() {   //NaN practice
     document.getElementById("Test").innerHTML = 0/0;
 }
-document.getElementById("Test").innerHTML = isNaN("Is this a string or not");
-document.getElementById("Test").innerHTML = isNaN("435")
-document.write(3E310);
-document.write(-3E310);
-document.write(500 > 10);
-document.write(500 < 10);
-console.log(5 + 5)
-document.write(50 == 50);
-document.write(50 == 25);
+document.getElementById("Test").innerHTML = isNaN("Is this a string or not"); //isNaN practice
+document.getElementById("Test").innerHTML = isNaN("435") //isNan practice
+document.write(3E310); //Showing infinity
+document.write(-3E310); //Negative infinity
+document.write(500 > 10); //True/False
+document.write(500 < 10); //Boolean
+console.log(5 + 5) //Console Log Practice
+document.write(50 == 50); //Checking to see if equal
+document.write(50 == 25); //Checking to see if equal
 X = 15
 Y = 15
-document.write(X === Y);
+document.write(X === Y); //Checking if equal variable AND type
 X = 22
 Y = "22"
-document.write(X === Y);
+document.write(X === Y); //Checking if equal variable AND type
 X = 46
 Y = 52
-document.write(x === Y);
-X = 57
+document.write(x === Y); //Checking if equal variable AND type
+X = 97
 Y = "58"
 document.write(X === Y);
-document.write(5 > 4 && 88 > 77);
-document.write(5 > 77 && 88 > 77);
-document.write(5 > 11 || 17 > 13);
-document.write(5 > 55 || 17 > 22);
+document.write(5 > 4 && 88 > 77); //And logic
+document.write(5 > 77 && 88 > 77); //And logic
+document.write(5 > 11 || 17 > 13); //Or logic
+document.write(5 > 55 || 17 > 22); //Or logic
 function not_Function() {
     document.getElementById("Not").innerHTML = !(20>10);
 }
