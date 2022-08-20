c0dec9cf210bcbfd7b78e5a8b74057acaf7fc8b2
Jwise777 <109560319+Jwise777@users.noreply.github.com>
2022-08-20 00:43:35 -0400

    Project5

--- Project5_comparisons_type_coercion/JS/main.js
+++ Project5_comparisons_type_coercion/JS/main.js
@@ -1,1 +1,36 @@
 document.write(typeof 7);
+document.write("40" + 10);
+function my_Function() {
+    document.getElementById("Test").innerHTML = 0/0;
+}
+document.getElementById("Test").innerHTML = isNaN("Is this a string or not");
+document.getElementById("Test").innerHTML = isNaN("435")
+document.write(3E310);
+document.write(-3E310);
+document.write(500 > 10);
+document.write(500 < 10);
+console.log(5 + 5)
+document.write(50 == 50);
+document.write(50 == 25);
+X = 15
+Y = 15
+document.write(X === Y);
+X = 22
+Y = "22"
+document.write(X === Y);
+X = 46
+Y = 52
+document.write(x === Y);
+X = 57
+Y = "58"
+document.write(X === Y);
+document.write(5 > 4 && 88 > 77);
+document.write(5 > 77 && 88 > 77);
+document.write(5 > 11 || 17 > 13);
+document.write(5 > 55 || 17 > 22);
+function not_Function() {
+    document.getElementById("Not").innerHTML = !(20>10);
+}
+function not_Function() {
+    document.getElementById("Not").innerHTML = !(20>33);
+}
