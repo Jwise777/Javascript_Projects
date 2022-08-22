675d22512d8ac2ae70eb81bf4771b2628432207f
Jwise777 <109560319+Jwise777@users.noreply.github.com>
2022-08-22 00:03:10 -0400

    Project 7 practice

---
+++ Project7_scope_time_function/JS/main.js
@@ -0,0 +1,49 @@
+var X = 55;  //Using outside/global variable to accomplish 2 math functions
+function Add_numbers_1() {
+    document.write(20 + X + "<br>");
+}
+function Add_numbers_2() {
+    document.write(X + 100);
+}
+Add_numbers_1();
+Add_numbers_2();
+function Add_numbers_3() { //Using a local variable to intentionally miss the 2nd function
+    var Y = 77;
+    document.write(20 + Y + "<br>");
+}
+function Add_numbers_4() {
+    document.write(Y + 100);
+}
+Add_numbers_3();
+Add_numbers_4();
+function Add_numbers_3() { //Using console log to assist in explaining where I went wrong in the code
+    var Y = 77;
+    console.log(15 + Y);
+}
+function Add_numbers_3() {
+    console.log(Y + 100);
+}
+Add_numbers_3();
+Add_numbers_4();
+
+function get_Date(); { //Function to pull the date and hours to send a message if before 6PM
+    if (new Date().getHours() < 18) {
+        document.getElementById("Greeting").innerHTML = "How are you today?";
+    }
+}
+
+function guess_number(); { //Creating a function for "if" statement 
+    if (number == 7) {
+        document.getElementById("Guess").innerHTML = "Great guess!";
+    }
+}
+function Height_Function() { //Creating another function to run full "if" "else" statements and outputs
+    Height = docoument.getElementById("Height").value;
+    if (Height >= 70) {
+        Ride = "You are tall enough to ride!";
+    }
+    else {
+        Ride = "Oops, a little to short for this one!";
+    }
+    document.getElementById("How_tall_are_you?").innerHTML = Ride;
+}
